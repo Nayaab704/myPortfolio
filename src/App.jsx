e214c717f716a3125401fc28c89cdb80674622e5
pickthisalgo <nayaabshaikh704@gmail.com>
2023-10-20 09:14:43 +0530

    Added the popup resume1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import { useEffect, useState } from "react";
+import DownloadCV from "./components/DownloadCV";
+//const App = () => {
+//adding new code 
 
-const App = () => {
+function App() {
+  let [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsOpen(true);
+    }, 40000);
+  }, []);
+
+
+//ending 
 
   return (
     <BrowserRouter>
@@ -10,6 +24,7 @@ const App = () => {
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
+          <DownloadCV isOpen={isOpen} setIsOpen={setIsOpen} />
         </div>
         <About />
         <Experience />
